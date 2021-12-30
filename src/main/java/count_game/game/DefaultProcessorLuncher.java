1fae840e1558272de08b09d82df5d50f0ec8fc44
amwtke <amwtke@gmail.com>
2021-12-30 15:12:50 +0800

    refactor

--- src/main/java/count_game/game/DefaultProcessorLuncher.java
+++ src/main/java/count_game/game/DefaultProcessorLuncher.java
@@ -15,8 +15,4 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DefaultProcessorLuncher extends DefaultProcessorService {
-    public <R, C extends DefaultProcessorContext<R>, P extends DefaultProcessor<R, C>>
-    DefaultProcessorResult<R> run(List<P> processors, C defaultContext) {
-        return runProcessors(processors, defaultContext);
-    }
 }
